1236fedecde59358d51e364d4af9bf4ff5ae4bce
songyuheon98 <140541167+songyuheon98@users.noreply.github.com>
2023-08-29 08:48:36 +0900

    Update MainRepository.java

--- src/main/java/com/hanghae/springsollevel1/repository/MainRepository.java
+++ src/main/java/com/hanghae/springsollevel1/repository/MainRepository.java
@@ -3,6 +3,6 @@ package com.hanghae.springsollevel1.repository;
 import com.hanghae.springsollevel1.entity.LevelOneData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MainRepository extends JpaRepository<LevelOneData,Long> { // memoRepository
+public interface MainRepository extends JpaRepository<LevelOneData,Long> { 
 
 }
