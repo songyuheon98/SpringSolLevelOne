7496592078d98896e0f19586002aed95f9d47d9d
songyuheon98 <songyuheon2750@gmail.com>
2023-08-29 16:30:05 +0900

    SPring Jpa로 재구성

---
+++ src/main/java/com/hanghae/springsollevel1/dto/LevelOneDataRequestPullDto.java
@@ -0,0 +1,14 @@
+package com.hanghae.springsollevel1.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LevelOneDataRequestPullDto {
+
+    private String title;
+    private String author;
+    private String contents;
+    private String pw;
+}
